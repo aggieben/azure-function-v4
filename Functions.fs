60ee0058c25b3cbc9f0d820b3c5fe3df33356239
Benjamin Collins <aggieben@gmail.com>
2021-11-17 13:11:22 -0600

    changes to support blob input; also test log levels

--- Functions.fs
+++ Functions.fs
@@ -3,15 +3,20 @@ module azure_function_v4.Functions
 open System.Net
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Azure.Functions.Worker.Http
+open Microsoft.Azure.WebJobs
 open Microsoft.Extensions.Logging
+open Azure.Storage.Blobs
 
 [<Function("Index")>]
 let run ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = null)>]req: HttpRequestData)
         (context:FunctionContext)
+        ([<Blob("test-blobs")>] containerClient:BlobContainerClient)
     =
     async {
         let log = context.GetLogger("azure_function_v4.Functions.Index")
         log.LogInformation("F♯ Http trigger function received a request.")
+
+        log.LogDebug("Container client: {0}", containerClient)
 
         let response = req.CreateResponse(HttpStatusCode.OK)
         do! response.WriteStringAsync("function executed successfully") |> Async.AwaitTask
